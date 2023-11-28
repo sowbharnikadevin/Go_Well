16e070bc6ab0075f18548da8ff3d786804efb34a
sowbharnikadevin <727722eucd049@skcet.ac.in>
2023-11-28 10:22:22 +0530

    sa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import LoginTemplate from './LoginTemplate';
+import MyReg from './MyReg';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <LoginTemplate/>
+    <MyReg/>
   </React.StrictMode>
 );
 
